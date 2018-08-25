8fa0dc94f6869eba53b287ae0742b1740dc07b24
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-25 10:53:42 -0700

    ForLoops

---
+++ src/ForLoops.java
@@ -0,0 +1,25 @@
+
+public class ForLoops {
+public static void main(String[] args) {
+	for (int i = 0; i < 101; i++) {
+		System.out.println(i);
+	}
+	for (int i = 100; i >0; i++) {
+		System.out.println(i);
+	}
+	for (int i = 2; i < 101; i+=2) {
+		System.out.println();
+	}
+	for (int i = 1; i < 99; i+=2) {
+		System.out.println();
+	}
+	for (int i = 1; i < 501; i++) {
+		if(i%2==0);{
+		System.out.println(i+" is eveeeennn");
+	}
+	else {
+		System.out.println(i+" is oddddd");
+	}
+	}
+}
+}
