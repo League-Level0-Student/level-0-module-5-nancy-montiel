67c368a01729e83bc0bef74a8dfb9e0b6f3746ac
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-08 10:50:55 -0700

    ForLoops

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -16,8 +16,10 @@ public static void main(String[] args) {
 	for (int i = 1; i < 501; i++) {
 		if(i%2==0){
 		System.out.println(i+" is eveeeennn");
-	}else{
+	}else {
+		
 		System.out.println(i+" is oddddd");
+	}
 	}
 	for (int j = 0; j < 784; j+=7) {
 			System.out.println(j);
@@ -27,6 +29,29 @@ public static void main(String[] args) {
 		System.out.println(j+ " I was "+age);
 		age++;
 	}
+	for (int j = 0; j < 7; j+=3) {
+	for (int i = 1; i < 4; i++) {
+		System.out.print(i+j);
+		
 	}
-}
-}
+	System.out.println();
+		}
+	for (int i = 0; i < 91; i+=10) {
+		for (int j = 1; j < 11; j++) {
+			System.out.print(i+j);
+		}
+		System.out.println();
+			
+		}
+	for (int i = 0; i < 1; i+=1) {
+		System.out.println("*");
+			}
+	for (int i = 0; i < 2; i+=2) {
+		System.out.println("*");
+		
+	}
+		
+	}
+	}
+
+
