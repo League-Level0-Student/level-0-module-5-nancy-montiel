d61e6f5712ba101777bcd940177c185456a84ad7
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-08 10:51:36 -0700

    ForLoops

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -46,9 +46,7 @@ public static void main(String[] args) {
 	for (int i = 0; i < 1; i+=1) {
 		System.out.println("*");
 			}
-	for (int i = 0; i < 2; i+=2) {
-		System.out.println("*");
-		
+	
 	}
 		
 	}
